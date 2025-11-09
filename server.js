56f4aa7a580b2ed185045089989f2bd2c5a63b28
Zakhariy <Zakhar.Struminskiy@gmail.com>
2025-11-09 13:42:15 +0200

    realised post method, alsi updated add.html

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 const port = 3000;
 
+
 app.use((req, res, next) => {
     const token = req.query.token;
     console.log(`The token is: ${token}`);
@@ -19,7 +20,10 @@ app.use((req, res, next) => {
     next()    
 })
 
+app.use(express.static(__dirname + `/public`));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/tasks', (req, res) => {
     const filePath = path.join(__dirname, 'api', 'data', 'tasks.json');
@@ -39,6 +43,30 @@ app.get('/tasks', (req, res) => {
             res.status(500).send('Could not parse data file');
         }
     })})
+
+app.post('/tasks/add', (req, res) => {
+  const filePath = path.join(__dirname, 'api', 'data', 'tasks.json');
+  const { title, description } = req.body;
+  const isCompleted = req.body.completed === 'true';
+
+  const data = fs.readFileSync(filePath, 'utf-8');
+  const tasks = JSON.parse(data);
+
+  const maxId = tasks.length > 0 ? Math.max(...tasks.map(t => t.id)) : 0;
+
+  const newTask = {
+    id: maxId + 1,
+    title,
+    description,
+    completed: isCompleted
+  };
+
+  tasks.push(newTask);
+  fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+
+  res.status(201).json({ message: 'Task added', task: newTask });
+});
+
 
 
 
